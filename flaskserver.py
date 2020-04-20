32e5fe6e162f8ba6010e9140244b092937584d18
Amgad-Abdelkhaleq <amgad.abdelkhaleq@gmail.com>
2020-04-20 15:21:41 +0200

    add button upload

--- flaskserver.py
+++ flaskserver.py
@@ -19,16 +19,15 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/get",methods=['POST','GET'])
+@app.route("/chat",methods=['POST','GET'])
 def get_bot_response():
     if request.method == "POST":
-        if(userText=="upload"):
-            UPLOAD_FOLDER =os.path.join(os.getcwd(),"static/images/text-based")
-            app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-            app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
-            # Get the name of the uploaded files
-            uploaded_files = request.files.getlist("file[]")
-            for file in uploaded_files:
+        UPLOAD_FOLDER =os.path.join(os.getcwd(),"static/images/text-based")
+        app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+        app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
+        # Get the name of the uploaded files
+        uploaded_files = request.files.getlist("file[]")
+        for file in uploaded_files:
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -37,13 +36,13 @@ def get_bot_response():
                 data_dict= {"image":filename ,"body":text}
                 fname =  os.path.join(os.getcwd(),"static\KB\output.json")
                 if os.path.isfile(fname):
-                # File exists
-                with open(fname, 'a+') as outfile:
-                    outfile.seek(-1, os.SEEK_END)
-                    outfile.truncate()
-                    outfile.write(',')
-                    json.dump(data_dict, outfile)
-                    outfile.write(']')
+                    # File exists
+                    with open(fname, 'a+') as outfile:
+                        outfile.seek(-1, os.SEEK_END)
+                        outfile.truncate()
+                        outfile.write(',')
+                        json.dump(data_dict, outfile)
+                        outfile.write(']')
 
     elif request.method == "GET":
         userText = request.args.get('msg')
@@ -63,21 +62,21 @@ def get_bot_response():
         #     pass
         #result={"Answer":text,"image_name":image_str,"type":"tag"}
         else:
-        newdata = {"question": userText} # this is the new data you're going to send to the Node server
-        # now immediately sending a post request with new data
-        try:
-            post = requests.post('http://localhost:3000/postdata', json=newdata) # the POST request      
-            print(">>>>>>>>>flask recived this :",post.text)
-            result=json.loads(post.text) 
-            return result
-        except:
-            print("i failed connecting node")
+            newdata = {"question": userText} # this is the new data you're going to send to the Node server
+            # now immediately sending a post request with new data
+            try:
+                post = requests.post('http://localhost:3000/postdata', json=newdata) # the POST request      
+                print(">>>>>>>>>flask recived this :",post.text)
+                result=json.loads(post.text) 
+                return result
+            except:
+                print("i failed connecting node")
       
        
 
 
 if __name__ == "__main__":
-   app.run(host='localhost', port=9960)
+   app.run(host='localhost', port=8763)
 
 
 
